f45b5a5ad82b14c5f6145d155e4e2d6eede7b83f
zhouzhenfei8888 <8zzf@sina.com>
2015-04-07 00:19:12 +0800

    student

--- AppStudentClient/src/com/fclassroom/activity/SearchActivity.java
+++ AppStudentClient/src/com/fclassroom/activity/SearchActivity.java
@@ -2,17 +2,54 @@ package com.fclassroom.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.fclassroom.appstudentclient.R;
 
 public class SearchActivity extends BaseActivity {
-
+    private static String data[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
+            "iPhone 4S", "Samsung Galaxy Note 800",
+            "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        initViews();
+    }
+
+    private void initViews() {
+        editText = (EditText)findViewById(R.id.et_search);
+        listView = (ListView)findViewById(R.id.lv_searchrecoder);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(arrayAdapter);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                listView.setVisibility(View.VISIBLE);
+                SearchActivity.this.arrayAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0){
+                    listView.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
 
