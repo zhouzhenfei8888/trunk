96f334327818d2adc32d330abcbb65a7155f7dbe
zhouzhenfei8888 <8zzf@sina.com>
2015-04-08 19:03:00 +0800

    student

--- AppStudentClient/src/com/fclassroom/activity/AddwrongtagActivity.java
+++ AppStudentClient/src/com/fclassroom/activity/AddwrongtagActivity.java
@@ -74,9 +74,11 @@ public class AddwrongtagActivity extends BaseActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if(actionId == EditorInfo.IME_ACTION_DONE){
-                    UIHelper.ToastMessage(AddwrongtagActivity.this,"ok");
+//                    UIHelper.ToastMessage(AddwrongtagActivity.this,"kkkkkkkkkkkkkkkkkkkkkkk");
+                    System.out.println("kkkkkkk");
+                    return true;
                 }
-                return true;
+                return false;
             }
         });
     }
